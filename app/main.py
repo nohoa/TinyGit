550d8803b636bbd06906e55844baa93c154d4b7d
chomotngayloihuanohoa <101667420+chomotngayloihuanohoa@users.noreply.github.com>
2025-02-04 13:22:13 +0700

    Add hash object directory

--- app/main.py
+++ app/main.py
@@ -23,14 +23,11 @@ def main():
         dest = obj[2:]
         final_path = ".git/objects/" + path+"/"+dest +"/"
         final_path += os.listdir(final_path)[0]
-        #print(final_path)
-        current_content = ""
-        #print(os.listdir(final_path))
+
         with open(final_path,"rb") as f : 
             decompressed  = zlib.decompress(f.read())
             string_object = decompressed.decode("utf-8")
-            current_content = string_object[8:].strip()
-            print(string_object[8:].strip(),end="")
+            print("Content of the file is : ",string_object[7:].strip(),end="")
     elif command == "hash-object" :
         if len(sys.argv) < 3 : 
             raise RuntimeWarning("Add more detail (-w) filename")
@@ -43,21 +40,21 @@ def main():
 
 def hashing(pathname) : 
     store = "";
-    print(pathname)
     with open(pathname, "rb") as fd : 
         data = fd.read()
         store = data.decode("utf-8")
-    result = b'blob' + b' ' + str(len(data)).encode() + b'\x00' + data
+        #print(store)
+    result = b'blob' + b' ' + str(len(data)).encode() + b'\x00' + store.encode("utf-8")
     hash_value = hashlib.sha1(result)
     hash_val = hash_value.hexdigest()
-    print(hash_val)
+
+    print("Hash value of blob object is: ",hash_val)
 
     gitpath =hash_val[0:2]
     dest = hash_val[2:]
     interpath = os.getcwd() +"/.git/objects/" 
-    #if not os.path.exists(os.path.join(interpath,path)) :
-    path1 = os.path.join(interpath,gitpath)
 
+    path1 = os.path.join(interpath,gitpath)
     if not os.path.exists(path1) :
         os.mkdir(path1)
 
